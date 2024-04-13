2a6e284f83b81546949fabff9170c6ef6b49d46d
Nilanjan Haldar <nil.haldar@gmail.com>
2024-04-13 18:31:46 +0530

    code

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
-// export function GET(request) {
-// 	return new Response
-// 	(`Hello from ${process.env.VERCEL_REGION}`);
+export function GET(request) {
+	return new Response
+	(`Hello from ${process.env.VERCEL_REGION}`);
 
-// }
+}
 
 // export function POST(request) {
 // 	return new Response
